7b6f162074f071d6b49d3ea8bf0e64447ba750ac
barteek-poz <szowior4@wp.pl>
2024-07-16 13:07:45 +0200

    created hero section

--- oddaj-rzeczy/src/app/layout.js
+++ oddaj-rzeczy/src/app/layout.js
@@ -1,10 +1,10 @@
 import MainNavigation from "@/_components/MainNavigation";
 import { Merriweather, Open_Sans } from "next/font/google";
-import  "../_styles/global.scss"
+import "../_styles/global.scss";
 import LoginNavigation from "@/_components/LoginNavigation";
 
 const merriweather = Merriweather({ subsets: ["latin"], weight: "400" });
-const openSans = Open_Sans({ subsets: ["latin"], weight: "400" });
+const openSans = Open_Sans({ subsets: ["latin"], weight:["300", "400"] });
 
 export const metadata = {
   title: "Oddaj rzeczy",
@@ -12,10 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" >
+    <html lang="en">
       <body className={openSans.className}>
-        <LoginNavigation/>
-        <MainNavigation />
+        <div style={{position: 'absolute', right:0, zIndex: 10}}>
+          <LoginNavigation />
+          <MainNavigation />
+        </div>
         <main>{children}</main>
       </body>
     </html>
